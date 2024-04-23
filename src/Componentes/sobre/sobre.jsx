4719835a4a3a45098d98fd0da904cf6871c82932
Emerson <emersoncovane23@gmail.com>
2024-04-23 13:56:19 +0200

    A finalizar a seccao sobre

--- src/Componentes/sobre/sobre.jsx
+++ src/Componentes/sobre/sobre.jsx
@@ -1,19 +1,18 @@
 import "../../Estilos/styleSobre.css";
-import instagramImg from '../../Imagens/instagram-1-svgrepo-com.svg';
-import facebookImg from '../../Imagens/facebook-color-svgrepo-com.svg';
-import linkedinImg from '../../Imagens/linkedin-svgrepo-com.svg';
-import gitHubImg from '../../Imagens/github-142-svgrepo-com.svg';
+import instagramImg from "../../Imagens/instagram-1-svgrepo-com.svg";
+import facebookImg from "../../Imagens/facebook-color-svgrepo-com.svg";
+import linkedinImg from "../../Imagens/linkedin-svgrepo-com.svg";
+import gitHubImg from "../../Imagens/github-142-svgrepo-com.svg";
 
-
-function Contacto(props){
+function Contacto(props) {
   return (
     <div id="cardSobreContactos">
-    <img id="cardContactoImg" src={props.gitHub} alt="" />
-    <img id="cardContactoImg" src={props.instagram} alt="" />
-    <img id="cardContactoImg" src={props.facebook} alt="" />
-    <img id="cardContactoImg" src={props.linkedin} alt="" />
-  </div>
-  )
+      <img id="cardContactoImg" src={props.gitHub} alt="" />
+      <img id="cardContactoImg" src={props.instagram} alt="" />
+      <img id="cardContactoImg" src={props.facebook} alt="" />
+      <img id="cardContactoImg" src={props.linkedin} alt="" />
+    </div>
+  );
 }
 
 function Card() {
@@ -23,7 +22,10 @@ function Card() {
         <div id="cardSobreFoto"></div>
       </div>
       <div id="cardSobreInfo">
-        
+        <p id="cardSobreText">Emerson Domingos Covane</p>
+        <p id="cardSobreText">Desenvolvedor web</p>
+        <p id="cardSobreText">Front End</p>
+        <p id="cardSobreText">Back End</p>
       </div>
     </div>
   );
@@ -31,33 +33,36 @@ function Card() {
 
 function DescricaoSobre() {
   return (
-    
     <div id="descricaoSobre">
-      
       <div id="textoLocSobre">
-      <h1 id="tituloTextoSobre">Quem sou eu?</h1>
-      <p id="textoSobre">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem,
-      temporibus fugiat magnam totam dicta quam minima. Eos repellendus est
-      tenetur mollitia accusantium, cumque unde similique ea asperiores quaerat
-      eum quos perspiciatis obcaecati laborum laboriosam ullam cum earum atque
-      doloremque placeat commodi itaque! Quam odio iste perferendis quas ad
-      architecto id!
-      </p>
+        <h1 id="tituloTextoSobre">Quem sou eu?</h1>
+        <p id="textoSobre">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem,
+          temporibus fugiat magnam totam dicta quam minima. Eos repellendus est
+          tenetur mollitia accusantium, cumque unde similique ea asperiores
+          quaerat eum quos perspiciatis obcaecati laborum laboriosam ullam cum
+          earum atque doloremque placeat commodi itaque! Quam odio iste
+          perferendis quas ad architecto id!
+        </p>
       </div>
-      
+
       <div id="textoLocSobre">
-      <h1 id="tituloTextoSobre">Quem sou eu?</h1>
-      <p id="textoSobre">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem,
-      temporibus fugiat magnam totam dicta quam minima. Eos repellendus est
-      tenetur mollitia accusantium, cumque unde similique ea asperiores quaerat
-      eum quos perspiciatis obcaecati laborum laboriosam ullam cum earum atque
-      doloremque placeat commodi itaque! Quam odio iste perferendis quas ad
-      architecto id!
-      </p>
+        <h1 id="tituloTextoSobre">Quem sou eu?</h1>
+        <p id="textoSobre">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem,
+          temporibus fugiat magnam totam dicta quam minima. Eos repellendus est
+          tenetur mollitia accusantium, cumque unde similique ea asperiores
+          quaerat eum quos perspiciatis obcaecati laborum laboriosam ullam cum
+          earum atque doloremque placeat commodi itaque! Quam odio iste
+          perferendis quas ad architecto id!
+        </p>
       </div>
-      <Contacto linkedin={linkedinImg} instagram={instagramImg} gitHub={gitHubImg} facebook={facebookImg} />
+      <Contacto
+        linkedin={linkedinImg}
+        instagram={instagramImg}
+        gitHub={gitHubImg}
+        facebook={facebookImg}
+      />
     </div>
   );
 }
