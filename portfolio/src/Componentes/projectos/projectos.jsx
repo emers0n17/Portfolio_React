5c44a7c26c766b07b5bd7a0731300d954e545e90
Emerson Covane <emersoncovane23@gmail.com>
2024-05-08 13:10:20 +0200

    Adicao dos efeitos do Swiper

--- portfolio/src/Componentes/projectos/projectos.jsx
+++ portfolio/src/Componentes/projectos/projectos.jsx
@@ -1,47 +1,67 @@
-import '../../Estilos/styleProjectos.css'
-import Projecto from './projecto';
-import background from '../../Imagens/background2.png'
+import "../../Estilos/styleProjectos.css";
+import Projecto from "./projecto";
+import background from "../../Imagens/background2.png";
+import { EffectCards } from "swiper/modules";
 
 //Swiper slide
-import { register } from 'swiper/element/bundle'
-import {Swiper, SwiperSlide} from 'swiper/react'
+import { register } from "swiper/element/bundle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow } from "swiper/modules";
 
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-cards";
 register();
 
 function Projectos() {
-    
-    const descricao = {
-        
-        projecto2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis repellendus beatae! Iste quis ea ratione.",
-        projecto3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis repellendus beatae! Iste quis ea ratione.",
-        projecto4: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis repellendus beatae! Iste quis ea ratione.",
-        projecto5: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis repellendus beatae! Iste quis ea ratione."
-    }
+  const slides = [
+    {
+      id: "1",
+      descricao:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis .",
+    },
+    {
+      id: "2",
+      descricao:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis .",
+    },
+    {
+      id: "3",
+      descricao:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis .",
+    },
+    {
+      id: "4",
+      descricao:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis .",
+    },
+    {
+      id: "5",
+      descricao:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis .",
+    },
+  ];
 
-    const slides = [
-        {id: "1", descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis ."},
-        {id: "2", descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis ."},
-        {id: "3", descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis ."},
-        {id: "4", descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis ."},
-        {id: "5", descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem illum laboriosam numquam rerum, obcaecati voluptatem aliquid facere, ad officiis maxime officia delectus alias enim sapiente doloribus repellendus. Recusandae quia explicabo dolor, corporis cumque nostrum quidem vero. Sit deleniti doloremque commodi rem, numquam reiciendis ."}
-    ]
-
-    return (
-        <div className="conatinerProjectos">
-            <div className="conteudoProjectos">
-            <Swiper className='swiper' slidesPerView={1} pagination= {{clickable: true}} navigation>
-                {slides.map((item) => (
-                    <SwiperSlide key={item.id}>
-                        <Projecto imagen={background} descricao={item.descricao}/>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-            </div>
+  return (
+    <div className="conatinerProjectos">
+      <div className="conteudoProjectos">
+        <div className="swiperProjecto">
+          <Swiper
+            className="swiperDoProjecto"
+            effect={"cards"}
+            modules={[EffectCards]}
+          >
+            <SwiperSlide><Projecto imagen={background} /></SwiperSlide>
+            <SwiperSlide><Projecto imagen={background} /></SwiperSlide>
+            <SwiperSlide><Projecto imagen={background} /></SwiperSlide>
+            <SwiperSlide><Projecto imagen={background} /></SwiperSlide>
+            <SwiperSlide><Projecto imagen={background} /></SwiperSlide>
+          </Swiper>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Projectos;
