162747b1cd688c3e28820de024923c1c3e0f7e4d
Emerson Covane <emersoncovane23@gmail.com>
2024-05-08 15:44:39 +0200

    Resolucao de erros para deploy

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom';
 import './Estilos/global.css';
 import Main from './Main';
+//Swiper slide
+import { register } from "swiper/element/bundle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-cards";
+register();
 
 
 
