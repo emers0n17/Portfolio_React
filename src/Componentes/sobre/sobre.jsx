c4fd8e75ec29d748bd80a1a35e8bcda798caa54e
Emerson Covane <emersoncovane23@gmail.com>
2024-05-13 00:05:42 +0200

    Adicao do Card perfil na seccao sobre

--- src/Componentes/sobre/sobre.jsx
+++ src/Componentes/sobre/sobre.jsx
@@ -1,80 +1,56 @@
 import "../../Estilos/styleSobre.css";
-// import instagramImg from "../../Imagens/instagram-white.svg";
-// import facebookImg from "../../Imagens/facebook-white.svg";
-// import linkedinImg from "../../Imagens/Linkedin_whtite.svg";
-// import gitHubImg from "../../Imagens/github-white.svg";
+import instagramImg from "../../Imagens/instagram-white.svg";
+import facebookImg from "../../Imagens/facebook-white.svg";
+import linkedinImg from "../../Imagens/Linkedin_whtite.svg";
+import gitHubImg from "../../Imagens/github-white.svg";
 
-
-// function Contacto(props) {
-//   return (
-//     <div id="cardSobreContactos">
-//       <img id="cardContactoImg" src={props.gitHub} alt="" />
-//       <img id="cardContactoImg" src={props.instagram} alt="" />
-//       <img id="cardContactoImg" src={props.facebook} alt="" />
-//       <img id="cardContactoImg" src={props.linkedin} alt="" />
-//     </div>
-//   );
-// }
-
-function Card() {
+function Perfil_card(){
   return (
-    <>
-    <div id="cardSobre">
-      <div id="cardSobreFotoPerfil">
-        <div id="cardSobreFoto"></div>
-      </div>
-      <div id="cardSobreInfo">
-        <p id="cardSobreText">Emerson Domingos Covane</p>
-        <p id="cardSobreText">Desenvolvedor web</p>
-        <p id="cardSobreText">Front End</p>
-        <p id="cardSobreText">Back End</p>
-      </div>
+    <div className="perfil_card">
+      <div className="card_info">
+        <img src={gitHubImg} alt="" />
+        <img src={instagramImg} alt="" />
+        <img src={facebookImg} alt="" />
+        <img src={linkedinImg} alt="" />
+        </div>
     </div>
-    </>
+  )
+}
+
+function Sobre_conteudo(){
+  return (
+    <div className="sobre_texto1">
+
+    </div>
+  )
+}
+
+function Sobre_text(){
+  return (
+    <div className="sobre_texto_container">
+      <Sobre_conteudo />
+      <button className="sobre_botao1">AutoBiografia</button>
+      <button className="sobre_botao2">Experiencias</button>
+      <button className="sobre_botao3">Ferramentas</button>
+    </div>
+  )
+}
+
+function Container_sobre() {
+  return (
+    <div className="card_container">
+      <Perfil_card />
+      <Sobre_text/>
+    </div>
   );
 }
 
-function DescricaoSobre() {
-    return (
-        <>
-            <div id="descricaoSobre">
-                {/*<div id="textoLocSobre">
-                    <h1 id="tituloTextoSobre">Biografia</h1>
-                    <p id="textoSobre">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem,
-                        temporibus fugiat magnam totam dicta quam minima. Eos repellendus est
-                        tenetur mollitia accusantium, cumque unde similique ea asperiores
-                        quaerat eum quos perspiciatis obcaecati laborum laboriosam ullam cum
-                        earum atque doloremque placeat commodi itaque! Quam odio iste
-                        perferendis quas ad architecto id!
-                    </p>
-                    
-                </div>
-
-<div id="textoLocSobre">
-    <h1 id="tituloTextoSobre">Quem sou eu?</h1>
-    <p id="textoSobre">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem,
-        temporibus fugiat magnam totam dicta quam minima. Eos repellendus est
-        tenetur mollitia accusantium, cumque unde similique ea asperiores
-        quaerat eum quos perspiciatis obcaecati laborum laboriosam ullam cum
-        earum atque doloremque placeat commodi itaque! Quam odio iste
-        perferendis quas ad architecto id!
-    </p>
-</div>
-<Contacto linkedin={linkedinImg} instagram={instagramImg} gitHub={gitHubImg} facebook={facebookImg}/> */}
-            </div>
-        </>
-    );
-}
 
 
 function Sobre() {
   return (
     <div id="sobreContainer">
-      
-      <div id="sobrePainel">
-      </div>
+      <Container_sobre />
     </div>
   );
 }
